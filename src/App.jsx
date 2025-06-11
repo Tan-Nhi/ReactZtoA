02f9d11c20d91383bb7eb20b0b3091dbe963e112
Nguyễn Tấn Nhi <144986314+Tan-Nhi@users.noreply.github.com>
2025-06-12 01:52:57 +0700

    video 29

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-// () => {}
-//component = html + csss + js
-const MyComponent = () => {
-  return (
-    <>
-      <div className="">Crocodile update</div>
-    </>
-  );
-}
+import MyComponent from './components/learn/MyComponent'
+import { SecondComponent, ThirdComponent } from './components/learn/SecondComponent'
 
 
 const App = () => {
@@ -29,6 +21,8 @@ const App = () => {
       </div>
       <h1>Hello World & Crocodile</h1>
       <MyComponent />
+      <SecondComponent />
+      <ThirdComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,11 @@
+// () => {}
+//component = html + csss + js
+const MyComponent = () => {
+    return (
+        <>
+            <div className="">Crocodile update</div>
+        </>
+    );
+}
+
+export default MyComponent;
