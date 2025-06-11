1c58bba7e2fe2fe92ad4e175ee2d696f0d9ec0ed
Nguyễn Tấn Nhi <144986314+Tan-Nhi@users.noreply.github.com>
2025-06-12 02:13:29 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,17 @@
 // () => {}
+//JSX  : 1 parent
 //component = html + csss + js
+import './style.css'
+
 const MyComponent = () => {
     return (
+        //fragment (<>)
         <>
             <div className="">Crocodile update</div>
+            <div className="child"
+                style={
+                    { borderRadius: "10px" }
+                }>child</div>
         </>
     );
 }
