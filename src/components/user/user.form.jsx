c1a916053988333dc09cb3c571b8d85a7ea36f09
Nguyễn Tấn Nhi <144986314+Tan-Nhi@users.noreply.github.com>
2025-06-15 00:41:20 +0700

    video 73

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,6 +1,6 @@
-import { Button, Input } from "antd";
+import { Button, Input, notification } from "antd";
 import { useState } from "react";
-import axios from "axios";
+import { createUserAPI } from "../../services/api.service";
 
 const UserForm = () => {
     const [fullName, setFullName] = useState("");
@@ -9,17 +9,19 @@ const UserForm = () => {
     const [phone, setPhone] = useState("");
 
 
-    const handClickBtn = () => {
-        const URL_BACKEND = "http://localhost:8080/api/v1/user";
-        const data = {
-            fullName: fullName,
-            email: email,
-            password: password,
-            phone: phone
+    const handClickBtn = async () => {
+        const res = await createUserAPI(fullName, email, password, phone)
+        if (res.data) {
+            notification.success({
+                message: "Create user",
+                description: "Tạo user thành công!!"
+            });
+        } else {
+            notification.error({
+                message: "Error create user",
+                description: JSON.stringify(res.message)
+            });
         }
-        axios.post(URL_BACKEND, data)
-        console.log(">> check state", { fullName, email, password, phone });
-
     }
     return (
         <div className="user-form" style={{ margin: "20px 0" }}>
