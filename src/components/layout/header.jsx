92ccd14763221d00d96bf69ec0b2a31866183f57
Nguyễn Tấn Nhi <144986314+Tan-Nhi@users.noreply.github.com>
2025-06-20 03:10:02 +0700

    video 102

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,17 +1,39 @@
-import { AlignLeftOutlined, AliwangwangFilled, AliwangwangOutlined, AuditOutlined, HomeOutlined, LoginOutlined, SettingOutlined, UsergroupAddOutlined } from '@ant-design/icons';
-import { Menu } from "antd";
+import { AliwangwangOutlined, AuditOutlined, HomeOutlined, LoginOutlined, UsergroupAddOutlined } from '@ant-design/icons';
+import { Menu, message } from "antd";
 import { useContext, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { logoutAPI } from '../../services/api.service';
 import { AuthContext } from '../context/auth.context';
 
 const Header = () => {
     const [current, setCurrent] = useState('');
-
-    const { user } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const { user, setUser } = useContext(AuthContext);
 
     const onClick = (e) => {
         setCurrent(e.key);
     };
+
+    const handleLogout = async () => {
+        const res = await logoutAPI();
+        if (res.data) {
+            //clear data 
+            localStorage.removeItem("access_token");
+            setUser({
+                email: "",
+                phone: "",
+                fullName: "",
+                role: "",
+                avatar: "",
+                id: ""
+            })
+            message.success("Đăng xuất thành công!");
+
+            //redirect to home page
+            navigate("/");
+        }
+    }
+
     const items = [
         {
             label: <Link to={"/"}>Home</Link>,
@@ -43,7 +65,7 @@ const Header = () => {
             icon: <AliwangwangOutlined />,
             children: [
                 {
-                    label: 'Đăng xuất',
+                    label: <span onClick={() => handleLogout()}> Đăng xuất</span>,
                     key: 'logout',
                 },
             ],
